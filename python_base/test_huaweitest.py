846a98982d2c3572a937694afe086e027781c237
liuyifei <liuyifeiflying1991@outlook.com>
2021-07-12 07:44:46 +0800

    update code

--- python_base/test_huaweitest.py
+++ python_base/test_huaweitest.py
@@ -676,17 +676,77 @@ HJ35 蛇形矩阵
 4 8
 7
 """
-while True:
-    try:
-        num_01 = int(input())
-        s = 1
-        for i in range(num_01):
-            s = s + i
-            print(s, end=' ')
-            p = s
-            for j in range(i + 1, num_01):
-                p = p + j + 1
-                print(p, end=' ')
-            print()
-    except:
-        break
+# while True:
+#     try:
+#         num_01 = int(input())
+#         s = 1
+#         for i in range(num_01):
+#             s = s + i
+#             print(s, end=' ')
+#             p = s
+#             for j in range(i + 1, num_01):
+#                 p = p + j + 1
+#                 print(p, end=' ')
+#             print()
+#     except:
+#         break
+"""
+HJ36 字符串加密
+描述
+有一种技巧可以对数据进行加密，它使用一个单词作为它的密匙。下面是它的工作原理：首先，选择一个单词作为密匙，如TRAILBLAZERS。如果单词中包含有重复的字母，只保留第1个，其余几个丢弃。现在，修改过的那个单词属于字母表的下面，如下所示：
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+T R A I L B Z E S C D F G H J K M N O P Q U V W X Y
+上面其他用字母表中剩余的字母填充完整。在对信息进行加密时，信息中的每个字母被固定于顶上那行，并用下面那行的对应字母一一取代原文的字母(字母字符的大小写状态应该保留)。因此，使用这个密匙，Attack AT DAWN(黎明时攻击)就会被加密为Tpptad TP ITVH。
+请实现下述接口，通过指定的密匙和明文得到密文。
+本题有多组输入数据。
+输入描述：
+先输入key和要加密的字符串
+nihao
+ni
+输出描述：
+返回加密后的字符串
+le
+"""
+# while True:
+#     try:
+#         key = input()
+#         pass_01 = input()
+#         alpha = 'abcdefghijklmnopqrstuvwxyz'
+# #                nihaobcdefgjklmpqrstuvwxyz
+#         pass_02 = ''
+#         alpha_conv = ''
+#         for i in key:
+#             if i not in alpha_conv:
+#                 alpha_conv += i
+#         for i in alpha:
+#             if i not in alpha_conv:
+#                 alpha_conv += i
+#         for i in pass_01:
+#             num_01 = alpha.find(i)
+#             pass_02 = pass_02 + alpha_conv[num_01]
+#         print(pass_02)
+#     except:
+#         break
+"""
+HJ37 统计每个月兔子的总数
+有一只兔子，从出生后第3个月起每个月都生一只兔子，小兔子长到第三个月后每个月又生一只兔子，假如兔子都不死，问每个月的兔子总数为多少？
+本题有多组数据。
+输入描述：
+输入int型表示month
+9
+输出描述：
+输出兔子总数int型
+34
+"""
+# while True:
+#     try:
+#         month=int(input())
+#         n=month-1
+#         def func(n):
+#             if n<2:#基线条件
+#                 return 1
+#             else:#递归条件
+#                 return func(n-1)+func(n-2)
+#         print(func(n))
+#     except:
+#         break
